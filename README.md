59fa6b6122f80419bf3ee91e102ec2256a611c14
manavgopal <gopal.1@iitj.ac.in>
2022-09-17 01:16:48 +0530

    added comments and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-hello
+# Youtube Video Search App
+This is an application which uses the Youtube API to fetch latest videos sorted in reverse chronological order of their publishing date-time from YouTube for a given tag/search query in a paginated response. <br />
+<br />
+How to run the application :<br />
+- Clone the Repo
+- Open the index.html in the live server
+
+How to search for a Query :<br />
+- Give a query in the search box
+- Select the Duration of the videos to be fetched
+- Sected the order type in which the videos to be ordered (default = date)
+- Select the Before date of the publication of the videos
+- Select the after date of the publication of the videos
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
+    // Initilization of the variables
     var $pagination = $("#pagination"),
       totalRecords = 0,
       records = [],
       recPerPage = 0,
       nextPageToken = "",
       totalPages = 0;
-    var API_KEY = "";
+    var API_KEY = config.API_KEY;
     var search = "";
     var duration = "any";
     var order = "date";
@@ -15,39 +16,44 @@ $(document).ready(function () {
   
     $("#beforedate").val(beforedate)
     $("#afterdate").val(afterdate)
-  
+
+    // This function gets invoked whenever the beforeDate is changed
     $("#beforedate").change(function(){
         beforedate = new Date(this.val()).toISOString()
         $("#beforedate").val(beforedate)
         afterdate = new Date(this.val()).toISOString()
         $("#afterdate").val(afterdate)
     })
-  
+
+    // This function gets invoked whenever the afterDate is changed
     $("#afterdate").change(function(){
       afterdate = new Date(this.val()).toISOString()
       $("#afterdate").val(afterdate)
       beforedate = new Date(this.val()).toISOString()
       $("#beforedate").val(beforedate)
     })
-  
+
+    // This function gets triggered whenever the duration is changed
     $("#duration").change(function () {
       duration = $(this).children("option:selected").val();
     });
     
+    // This function gets invoked when the user chanegs the order by which the videos should be fetched
     $("#order").change(function () {
       order = $(this).children("option:selected").val();
     });
-  
+
+    // Function which gets executed when the submit button is clicked
     $("#myForm").submit(function (e) {
       e.preventDefault();
       search = $("#search").val();
       console.log(beforedate);
 
-      API_KEY = "AIzaSyA6VhN1vBk4-Fij3XbpX33aFfNzcTd7aG0";
-  
+      // Search url
       var url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
           &part=snippet&q=${search}&maxResults=${maxResults}&publishedAfter=${afterdate}&publishedBefore=${beforedate}&order=${order}&videoDuration=${duration}&type=video`;
-  
+
+      // Getting the response of the Url
       $.ajax({
         method: "GET",
         url: url,
@@ -62,26 +68,31 @@ $(document).ready(function () {
         },
       });
     });
-  
+
+    // Function to apply pagination
     function apply_pagination() {
       $pagination.twbsPagination({
         totalPages: totalPages,
         visiblePages: 5,
+        // Change on clicking the page number
         onPageClick: function (event, page) {
           console.log(event);
+          // Getting the indexes between which the records should be displayed
           displayRecordsIndex = Math.max(page - 1, 0) * recPerPage;
           endRec = displayRecordsIndex + recPerPage;
+
           console.log(displayRecordsIndex + "ssssssssss" + endRec);
           displayRecords = records.slice(displayRecordsIndex, endRec);
           generateRecords(recPerPage, nextPageToken);
         },
       });
     }
-  
+    // Whenever the search query is changed this function gets triggered
     $("#search").change(function () {
       search = $("#search").val();
     });
-  
+    
+    // Function to generate all the records 
     function generateRecords(recPerPage, nextPageToken) {
       var url2 = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
       &part=snippet&q=${search}&maxResults=${maxResults}&pageToken=${nextPageToken}&publishedBefore=${beforedate}&publishedAfter=${afterdate}&order=${order}&videoDuration=${duration}&type=video`;
@@ -100,7 +111,7 @@ $(document).ready(function () {
         },
       });
     }
-  
+    // Function to display all the videos from the search list
     function displayVideos(data) {
       recPerPage = data.pageInfo.resultsPerPage;
       nextPageToken = data.nextPageToken;
@@ -111,7 +122,8 @@ $(document).ready(function () {
       $("#search").val("");
       var videoData = "";
       $("#table").show();
-  
+
+      // Showing all the data in the tabular format
       data.items.forEach((item) => {
         videoData = `<tr>
                         <td>
